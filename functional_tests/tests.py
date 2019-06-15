12890b2a43a913a6491debf7b27ae694d779812d
Amy Jording <amy@amyjording.com>
2019-06-15 12:28:42 -0700

    Add selenium WebDriverException to functional_test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 	
@@ -12,10 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 	
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	#helper function
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Amy wants to create another to-do for her list and goes to
@@ -39,8 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# "1: Choreograph a dance" as an item in a to-do list.
 		inputbox.send_keys('Choreograph a dance')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_list_table('1: Choreograph a dance')
+		self.wait_for_row_in_list_table('1: Choreograph a dance')
 
 
 		# There is still a text box inviting her to add another item. She
@@ -48,13 +58,11 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Practice the dance each Sunday')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
 
 		# The page updatesybuttest again, and shows both items on her list
 
-		self.check_for_row_in_list_table('1: Choreograph a dance')
-		self.check_for_row_in_list_table('2: Practice the dance each Sunday')
+		self.wait_for_row_in_list_table('1: Choreograph a dance')
+		self.wait_for_row_in_list_table('2: Practice the dance each Sunday')
 
 		self.fail("Finish the test!")
 
